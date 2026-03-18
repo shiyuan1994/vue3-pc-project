3e1fd2bdb086e7e21f186651e4997e0040a7558e
shiyuan1994 <1136386227@qq.com>
2026-03-18 11:23:34 +0800

    fix:(sy) 添加ts声明文件

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Layout from '@/layout/index.vue'
+import Layout from '@/layout/Index.vue'
 import { useUserInfoStore } from '@/stores/user'
 
 const routes = [
   {
     path: '/login',
-    component: () => import('@/views/login.vue')
+    component: () => import('@/views/login.vue'),
   },
   {
     path: '/',
@@ -15,25 +15,25 @@ const routes = [
       {
         path: '/dashboard',
         component: () => import('@/views/dashboard.vue'),
-        meta: { title: '工作台' }
+        meta: { title: '工作台' },
       },
       {
         path: '/progressProject',
         component: () => import('@/views/progressProject.vue'),
-        meta: { title: '项目管理' }
+        meta: { title: '项目管理' },
       },
       {
         path: '/form',
         component: () => import('@/views/addForm.vue'),
-        meta: { title: '新建表单' }
-      }
-    ]
-  }
+        meta: { title: '新建表单' },
+      },
+    ],
+  },
 ]
 
 const router = createRouter({
   history: createWebHistory(),
-  routes
+  routes,
 })
 
 // 简单路由守卫
@@ -41,7 +41,8 @@ router.beforeEach((to, from, next) => {
   const store = useUserInfoStore()
   // console.log('store.token', store.token)
   // console.log(to, from)
-  if (to.path !== '/login' && !store.token) { // 不是跳登录页，没有登录用户token
+  if (to.path !== '/login' && !store.token) {
+    // 不是跳登录页，没有登录用户token
     next('/login')
   } else {
     next()
